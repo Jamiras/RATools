32a65e545b413ea84ba7c658042a779a506f4625
Brian Weiss <brian_weiss@hotmail.com>
2017-12-07 20:20:36 -0700

    update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("3 Dec 2017")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("7 Dec 2017")]
 
 [assembly: InternalsVisibleTo("RATools.Test")]
