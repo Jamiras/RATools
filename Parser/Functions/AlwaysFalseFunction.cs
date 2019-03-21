febca45352c3eefa892a7dd1af6ab78df3324404
Jamiras <jamirasmt@gmail.com>
2019-03-20 22:02:40 -0600

    convert integer constant comparisons to always_true or always_false (fixes #20)

--- Parser/Functions/AlwaysFalseFunction.cs
+++ Parser/Functions/AlwaysFalseFunction.cs
@@ -19,15 +19,19 @@ namespace RATools.Parser.Functions
                 return false;
             }
 
+            context.Trigger.Add(CreateAlwaysFalseRequirement());
+
+            result = null;
+            return true;
+        }
+
+        public static Requirement CreateAlwaysFalseRequirement()
+        {
             var requirement = new Requirement();
             requirement.Left = new Field { Size = FieldSize.Byte, Type = FieldType.Value, Value = 0 };
             requirement.Operator = RequirementOperator.Equal;
             requirement.Right = new Field { Size = FieldSize.Byte, Type = FieldType.Value, Value = 1 };
-
-            context.Trigger.Add(requirement);
-
-            result = null;
-            return true;
+            return requirement;
         }
     }
 }
