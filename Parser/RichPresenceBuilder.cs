b72bd144f3b759342420e6c6b065f6eaa891b062
João Silva <joao.gde.silva@gmail.com>
2019-07-26 23:14:49 +0100

    Support for fallback value in rich presence lookups
    As specified in the Rich Presence docs: If a lookup doesn't contain a mapping
    for a value, it will result in a blank (no space) value. You can change this
    behavior by adding a single line to the lookup mapping '*' to the desired
    fallback value.
    
    Added support for that in RATools. The lookup map simply has to include a
    "*" string constant key.

--- Parser/RichPresenceBuilder.cs
+++ Parser/RichPresenceBuilder.cs
@@ -11,16 +11,28 @@ namespace RATools.Parser
     [DebuggerDisplay("{DisplayString}")]
     public class RichPresenceBuilder
     {
+        private class Lookup
+        {
+            public Lookup(IDictionary<int, string> dict, string fallback)
+            {
+                Dict = dict;
+                Fallback = fallback;
+            }
+
+            public IDictionary<int, string> Dict { get; private set; }
+            public string Fallback { get; private set; }
+        };
+
         public RichPresenceBuilder()
         {
             _valueFields = new TinyDictionary<string, ValueFormat>();
-            _lookupFields = new TinyDictionary<string, IDictionary<int, string>>();
+            _lookupFields = new TinyDictionary<string, Lookup>();
             _conditionalDisplayStrings = new List<string>();
         }
 
         private List<string> _conditionalDisplayStrings;
         private TinyDictionary<string, ValueFormat> _valueFields;
-        private TinyDictionary<string, IDictionary<int, string>> _lookupFields;
+        private TinyDictionary<string, Lookup> _lookupFields;
 
         public string DisplayString { get; set; }
         public int Line { get; set; }
@@ -35,28 +47,34 @@ namespace RATools.Parser
             _valueFields[name] = format;
         }
 
-        public void AddLookupField(string name, IDictionary<int, string> dict)
+        public void AddLookupField(string name, IDictionary<int, string> dict, string fallback)
         {
-            _lookupFields[name] = dict;
+            _lookupFields[name] = new Lookup(dict, fallback);
         }
 
         internal ParseErrorExpression AddLookupField(string name, DictionaryExpression dict)
         {
             var tinyDict = new TinyDictionary<int, string>();
+            string fallback = null;
             foreach (var entry in dict.Entries)
             {
-                var key = entry.Key as IntegerConstantExpression;
-                if (key == null)
-                    return new ParseErrorExpression("key is not an integer", entry.Key);
-
                 var value = entry.Value as StringConstantExpression;
                 if (value == null)
                     return new ParseErrorExpression("value is not a string", entry.Value);
 
-                tinyDict[key.Value] = value.Value;
+                var key = entry.Key as IntegerConstantExpression;
+                if (key != null)
+                    tinyDict[key.Value] = value.Value;
+                else
+                {
+                    var strKey = entry.Key as StringConstantExpression;
+                    if (strKey == null || strKey.Value != "*")
+                        return new ParseErrorExpression("key is not an integer or \"*\"", entry.Key);
+                    fallback = value.Value;
+                }
             }
 
-            AddLookupField(name, tinyDict);
+            AddLookupField(name, tinyDict, fallback);
             return null;
         }
 
@@ -72,14 +90,20 @@ namespace RATools.Parser
                 builder.Append("Lookup:");
                 builder.AppendLine(lookup.Key);
 
-                var list = new List<int>(lookup.Value.Keys);
+                var list = new List<int>(lookup.Value.Dict.Keys);
                 list.Sort();
 
                 foreach (var key in list)
                 {
                     builder.Append(key);
                     builder.Append('=');
-                    builder.AppendLine(lookup.Value[key]);
+                    builder.AppendLine(lookup.Value.Dict[key]);
+                }
+
+                if (lookup.Value.Fallback != null)
+                {
+                    builder.Append("*=");
+                    builder.AppendLine(lookup.Value.Fallback);
                 }
 
                 builder.AppendLine();
