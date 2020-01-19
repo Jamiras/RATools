09325b3bb561a66d262ad847f5a73c7d37dbbe98
Jamiras <jamirasmt@gmail.com>
2020-01-19 09:53:23 -0700

    update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.3.0")]
-[assembly: AssemblyFileVersion("1.6.3.0")]
-[assembly: AssemblyInformationalVersion("23 Nov 2019")]
+[assembly: AssemblyVersion("1.6.4.0")]
+[assembly: AssemblyFileVersion("1.6.4.0")]
+[assembly: AssemblyInformationalVersion("19 Jan 2020")]
 
 [assembly: InternalsVisibleTo("RATools.Test")]
