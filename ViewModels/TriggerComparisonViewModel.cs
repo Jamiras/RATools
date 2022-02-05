d513acc34629fe8ab6b75ee045a7e59d22c3f147
Jamiras <32680403+Jamiras@users.noreply.github.com>
2022-02-05 08:45:18 -0700

    only require exact matching for alt groups (#284)

--- ViewModels/TriggerComparisonViewModel.cs
+++ ViewModels/TriggerComparisonViewModel.cs
@@ -13,16 +13,62 @@ namespace RATools.ViewModels
             var groups = new List<RequirementGroupViewModel>();
             var emptyRequirements = new Requirement[0];
 
+            // first pass, match non-alt groups by name
+            var matches = new Dictionary<RequirementGroupViewModel, RequirementGroupViewModel>();
             foreach (var triggerGroup in trigger.Groups)
             {
+                if (triggerGroup.Label.StartsWith("Alt"))
+                    continue;
+
+                var compareTriggerGroup = compareTriggerGroups.FirstOrDefault(t => t.Label == triggerGroup.Label);
+                if (compareTriggerGroup != null)
+                {
+                    matches[triggerGroup] = compareTriggerGroup;
+                    compareTriggerGroups.Remove(compareTriggerGroup);
+                }
+            }
+
+            // second pass, match alt groups by exact content. alt group order doesn't matter.
+            foreach (var triggerGroup in trigger.Groups)
+            {
+                if (!triggerGroup.Label.StartsWith("Alt"))
+                    continue;
+
                 var compareTriggerGroup = compareTriggerGroups.FirstOrDefault(t => t.RequirementsAreEqual(triggerGroup));
                 if (compareTriggerGroup != null)
+                {
+                    matches[triggerGroup] = compareTriggerGroup;
+                    compareTriggerGroups.Remove(compareTriggerGroup);
+                }
+            }
+
+            // third pass, attempt to find close matches for remaining items
+            if (matches.Count != trigger.Groups.Count())
+            {
+                foreach (var triggerGroup in trigger.Groups)
+                {
+                    if (!matches.ContainsKey(triggerGroup))
+                    {
+                        var compareTriggerGroup = FindBestMatch(triggerGroup, compareTriggerGroups);
+                        if (compareTriggerGroup != null)
+                        {
+                            matches[triggerGroup] = compareTriggerGroup;
+                            compareTriggerGroups.Remove(compareTriggerGroup);
+                        }
+                    }
+                }
+            }
+
+            // create the comparison view models.
+            foreach (var triggerGroup in trigger.Groups)
+            {
+                RequirementGroupViewModel compareTriggerGroup;
+                if (matches.TryGetValue(triggerGroup, out compareTriggerGroup))
                 {
                     groups.Add(new RequirementGroupViewModel(triggerGroup.Label,
                         triggerGroup.Requirements.Select(r => r.Requirement),
                         compareTriggerGroup.Requirements.Select(r => r.Requirement),
                         numberFormat, notes));
-                    compareTriggerGroups.Remove(compareTriggerGroup);
                 }
                 else
                 {
@@ -32,6 +78,7 @@ namespace RATools.ViewModels
                 }
             }
 
+            // append any compared view models that weren't used
             foreach (var compareTriggerGroup in compareTriggerGroups)
             {
                 groups.Add(new RequirementGroupViewModel(compareTriggerGroup.Label, emptyRequirements,
@@ -40,6 +87,41 @@ namespace RATools.ViewModels
             }
 
             Groups = groups.ToArray();
+        }
+
+        private RequirementGroupViewModel FindBestMatch(RequirementGroupViewModel needle, IEnumerable<RequirementGroupViewModel> haystack)
+        {
+            RequirementGroupViewModel bestMatch = null;
+            int bestMatchCount = 0;
+            var requirementExs = RequirementEx.Combine(needle.Requirements.Select(r => r.Requirement));
+
+            foreach (var potentialMatch in haystack)
+            {
+                // convert to requirement clauses and compare
+                var compareRequirementExs = RequirementEx.Combine(potentialMatch.Requirements.Select(r => r.Requirement));
+                int matchCount = compareRequirementExs.Count;
+
+                foreach (var requirementEx in requirementExs)
+                {
+                    for (int i = 0; i < compareRequirementExs.Count; i++)
+                    {
+                        if (compareRequirementExs[i] == requirementEx)
+                        {
+                            compareRequirementExs.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+
+                matchCount -= compareRequirementExs.Count;
+                if (matchCount > bestMatchCount)
+                {
+                    bestMatchCount = matchCount;
+                    bestMatch = potentialMatch;
+                }
+            }
+
+            return bestMatch;
         }
     }
 }
