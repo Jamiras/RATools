649aa3eb077c8a9bdd579d7249e23476e4fa5cfb
Jamiras <32680403+Jamiras@users.noreply.github.com>
2022-03-30 08:56:47 -0600

    support for generating disable_when syntax for new scripts (#295)

--- Tests/Data/RequirementExTests.cs
+++ Tests/Data/RequirementExTests.cs
@@ -11,6 +11,10 @@ namespace RATools.Test.Data
     {
         [Test]
         [TestCase("0xH001234=1", "byte(0x001234) == 1")]
+        [TestCase("P:0xH001234=1", "unless(byte(0x001234) == 1)")]
+        [TestCase("R:0xH001234=1", "never(byte(0x001234) == 1)")]
+        [TestCase("0xH001234=1.1.", "once(byte(0x001234) == 1)")]
+        [TestCase("0xH001234=1.99.", "repeated(99, byte(0x001234) == 1)")]
         [TestCase("0xH001234=1_0xH002345=2", "byte(0x001234) == 1|byte(0x002345) == 2")]
         [TestCase("A:0xH001234=0_0xH002345=2", "(byte(0x001234) + byte(0x002345)) == 2")]
         [TestCase("A:0xH001234=0_R:0xH002345=2", "never((byte(0x001234) + byte(0x002345)) == 2)")]
@@ -30,7 +34,11 @@ namespace RATools.Test.Data
         [TestCase("N:1=1_0xH002345=2.15.", "repeated(15, byte(0x002345) == 2)")]
         [TestCase("N:0xH001234=1.15._1=1", "repeated(15, byte(0x001234) == 1)")]
         [TestCase("N:1=1.15._0xH002345=2", "(repeated(15, always_true()) && byte(0x002345) == 2)")]
-        public void TestCombine(string input, string expected)
+        [TestCase("M:0xH001234>=100", "measured(byte(0x001234) >= 100)")]
+        [TestCase("Z:0xH002345=2_0xH001234=1.2.", "repeated(2, byte(0x001234) == 1 && never(byte(0x002345) == 2))")]
+        [TestCase("Z:0xH002345=2_P:0xH001234=1.1.", "disable_when(byte(0x001234) == 1, until=byte(0x002345) == 2)")]
+        [TestCase("Z:0xH002345=2_P:0xH001234=1.2.", "disable_when(repeated(2, byte(0x001234) == 1), until=byte(0x002345) == 2)")]
+        public void TestAppendString(string input, string expected)
         {
             var achievement = new AchievementBuilder();
             achievement.ParseRequirements(Tokenizer.CreateTokenizer(input));
