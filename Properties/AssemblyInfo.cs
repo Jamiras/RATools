47083549a56182a4394d93e17b3e417a8cca0f08
Jamiras <jamirasmt@gmail.com>
2022-04-03 11:08:56 -0600

    update version to 1.8.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.4.0")]
-[assembly: AssemblyFileVersion("1.8.4.0")]
-[assembly: AssemblyInformationalVersion("15 Feb 2021")]
+[assembly: AssemblyVersion("1.8.5.0")]
+[assembly: AssemblyFileVersion("1.8.5.0")]
+[assembly: AssemblyInformationalVersion("3 Apr 2021")]
 
 [assembly: InternalsVisibleTo("RATools.Test")]
