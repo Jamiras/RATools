cec7aa1e852ac5ee0fc7f0c699f1143b8eda9f15
Jamiras <jamirasmt@gmail.com>
2022-06-13 06:49:02 -0600

    add ability to filter on users who unlocked one but not both achievements

--- Source/ViewModels/UnlockDistanceViewModel.cs
+++ Source/ViewModels/UnlockDistanceViewModel.cs
@@ -16,67 +16,89 @@ namespace RATools.ViewModels
     public class UnlockDistanceViewModel : DialogViewModelBase
     {
         public UnlockDistanceViewModel()
-            : this(ServiceRepository.Instance.FindService<IBackgroundWorkerService>(),
-                   ServiceRepository.Instance.FindService<IFileSystemService>(),
-                   ServiceRepository.Instance.FindService<ISettings>())
+            : this(ServiceRepository.Instance.FindService<IBackgroundWorkerService>())
         {
         }
 
-        public UnlockDistanceViewModel(IBackgroundWorkerService backgroundWorkerService, IFileSystemService fileSystem, ISettings settings)
+        public UnlockDistanceViewModel(IBackgroundWorkerService backgroundWorkerService)
         {
             _backgroundWorkerService = backgroundWorkerService;
-            _fileSystem = fileSystem;
-            _settings = settings;
 
             Progress = new ProgressFieldViewModel { Label = String.Empty };
             DialogTitle = "Unlock Distance";
             CanClose = true;
+
+            var comparisonLookup = new[]
+            {
+                new LookupItem((int)Comparison.Distance, "Distance"),
+                new LookupItem((int)Comparison.FirstWithoutSecond, "First without Second"),
+                new LookupItem((int)Comparison.SecondWithoutFirst, "Second without First"),
+            };
+
+            FirstAchievementId = new IntegerFieldViewModel("First Achievement ID", 0, 9999999);
+            SecondAchievementId = new IntegerFieldViewModel("Second Achievement ID", 0, 9999999);
+            ComparisonType = new LookupFieldViewModel("Comparison Type", comparisonLookup);
+            ComparisonType.SelectedId = (int)Comparison.Distance;
 
             SearchCommand = new DelegateCommand(Search);
             ShowUserUnlocksCommand = new DelegateCommand<UnlockInfo>(ShowUserUnlocks);
         }
 
         private readonly IBackgroundWorkerService _backgroundWorkerService;
-        private readonly IFileSystemService _fileSystem;
-        private readonly ISettings _settings;
         private int _gameId;
 
         public ProgressFieldViewModel Progress { get; private set; }
 
-        public static readonly ModelProperty FirstAchievementIdProperty = ModelProperty.Register(typeof(UnlockDistanceViewModel), "FirstAchievementId", typeof(int), 0);
+        public IntegerFieldViewModel FirstAchievementId { get; private set; }
+        public IntegerFieldViewModel SecondAchievementId { get; private set; }
+        public LookupFieldViewModel ComparisonType { get; private set; }
 
-        public int FirstAchievementId
+        public enum Comparison
         {
-            get { return (int)GetValue(FirstAchievementIdProperty); }
-            set { SetValue(FirstAchievementIdProperty, value); }
+            None = 0,
+            Distance,
+            FirstWithoutSecond,
+            SecondWithoutFirst,
         }
 
-
-        public static readonly ModelProperty SecondAchievementIdProperty = ModelProperty.Register(typeof(UnlockDistanceViewModel), "SecondAchievementId", typeof(int), 0);
-
-        public int SecondAchievementId
+        public static readonly ModelProperty FirstAchievementNameProperty = ModelProperty.Register(typeof(UnlockDistanceViewModel), "FirstAchievementName", typeof(string), "Unlock 1");
+        public string FirstAchievementName
         {
-            get { return (int)GetValue(SecondAchievementIdProperty); }
-            set { SetValue(SecondAchievementIdProperty, value); }
+            get { return (string)GetValue(FirstAchievementNameProperty); }
+            private set { SetValue(FirstAchievementNameProperty, value); }
+        }
+
+        public static readonly ModelProperty SecondAchievementNameProperty = ModelProperty.Register(typeof(UnlockDistanceViewModel), "SecondAchievementName", typeof(string), "Unlock 2");
+        public string SecondAchievementName
+        {
+            get { return (string)GetValue(SecondAchievementNameProperty); }
+            private set { SetValue(SecondAchievementNameProperty, value); }
+        }
+
+        public static readonly ModelProperty SummaryTextProperty = ModelProperty.Register(typeof(UnlockDistanceViewModel), "SummaryText", typeof(string), String.Empty);
+        public string SummaryText
+        {
+            get { return (string)GetValue(SummaryTextProperty); }
+            private set { SetValue(SummaryTextProperty, value); }
         }
 
         public CommandBase SearchCommand { get; private set; }
         private void Search()
         {
-            if (FirstAchievementId == 0)
+            if (FirstAchievementId.Value == 0)
             {
                 MessageBoxViewModel.ShowMessage("Invalid first achievement ID");
                 return;
             }
 
-            if (SecondAchievementId == 0)
+            if (SecondAchievementId.Value == 0)
             {
                 MessageBoxViewModel.ShowMessage("Invalid second achievement ID");
                 return;
             }
 
             Progress.Reset(2);
-            Progress.Label = "Fetching Achievement " + FirstAchievementId;
+            Progress.Label = "Fetching Achievement " + FirstAchievementId.Value.GetValueOrDefault();
             Progress.IsEnabled = true;
             _backgroundWorkerService.RunAsync(DoComparison);
         }
@@ -93,6 +115,9 @@ namespace RATools.ViewModels
             {
                 get
                 {
+                    if (FirstUnlock.Year < 2000 || SecondUnlock.Year < 2000)
+                        return "n/a";
+
                     var builder = new StringBuilder();
 
                     int totalMinutes = (int)Distance.TotalMinutes;
@@ -105,6 +130,28 @@ namespace RATools.ViewModels
                     builder.AppendFormat("{0}h{1:D2}m", totalMinutes / 60, totalMinutes % 60);
 
                     return builder.ToString();
+                }
+            }
+
+            public string FormattedFirstUnlock
+            {
+                get
+                {
+                    if (FirstUnlock.Year < 2000)
+                        return "n/a";
+
+                    return FirstUnlock.ToString();
+                }
+            }
+
+            public string FormattedSecondUnlock
+            {
+                get
+                {
+                    if (SecondUnlock.Year < 2000)
+                        return "n/a";
+
+                    return SecondUnlock.ToString();
                 }
             }
         }
@@ -137,14 +184,22 @@ namespace RATools.ViewModels
         private void DoComparison()
         {
             var firstUnlocks = new List<UnlockInfo>();
-            var unlocks = new List<UnlockInfo>();
 
-            var unlocksJson = RAWebCache.Instance.GetAchievementUnlocksJson(FirstAchievementId, 0);
-            if (unlocksJson != null)
+            var page = 0;
+            var count = 0;
+            do
             {
+                var unlocksJson = RAWebCache.Instance.GetAchievementUnlocksJson(FirstAchievementId.Value.GetValueOrDefault(), page++);
+                if (unlocksJson == null)
+                    break;
+
                 _gameId = unlocksJson.GetField("Game").ObjectValue.GetField("ID").IntegerValue.GetValueOrDefault();
+                FirstAchievementName = unlocksJson.GetField("Achievement").ObjectValue.GetField("Title").StringValue;
+
+                count = 0;
                 foreach (var unlock in unlocksJson.GetField("Unlocks").ObjectArrayValue)
                 {
+                    count++;
                     if (unlock.GetField("HardcoreMode").IntegerValue != 1)
                         continue;
 
@@ -156,33 +211,66 @@ namespace RATools.ViewModels
                     };
                     firstUnlocks.Add(unlockInfo);
                 }
-            }
+            } while (count == 500);
 
-            Progress.Label = "Fetching Achievement " + FirstAchievementId;
+            Progress.Label = "Fetching Achievement " + FirstAchievementId.Value.GetValueOrDefault();
             Progress.Current++;
 
-            unlocksJson = RAWebCache.Instance.GetAchievementUnlocksJson(SecondAchievementId, 0);
-            if (unlocksJson != null)
+            var compareType = (Comparison)ComparisonType.SelectedId;
+            var unlocks = (compareType == Comparison.FirstWithoutSecond) ? firstUnlocks : new List<UnlockInfo>();
+
+            page = 0;
+            do
             {
+                var unlocksJson = RAWebCache.Instance.GetAchievementUnlocksJson(SecondAchievementId.Value.GetValueOrDefault(), page++);
+                if (unlocksJson == null)
+                    break;
+
                 var gameId2 = unlocksJson.GetField("Game").ObjectValue.GetField("ID").IntegerValue.GetValueOrDefault();
                 if (gameId2 != _gameId)
                     _gameId = -1;
 
+                SecondAchievementName = unlocksJson.GetField("Achievement").ObjectValue.GetField("Title").StringValue;
+
+                count = 0;
+                UnlockInfo unlockInfo;
                 foreach (var unlock in unlocksJson.GetField("Unlocks").ObjectArrayValue)
                 {
+                    count++;
                     if (unlock.GetField("HardcoreMode").IntegerValue != 1)
                         continue;
 
                     string user = unlock.GetField("User").StringValue;
-                    var unlockInfo = firstUnlocks.FirstOrDefault(u => u.User == user);
-                    if (unlockInfo == null)
-                        continue;
 
-                    unlockInfo.SecondUnlock = unlock.GetField("DateAwarded").DateTimeValue.GetValueOrDefault();
-                    unlockInfo.Distance = unlockInfo.SecondUnlock - unlockInfo.FirstUnlock;
-                    unlocks.Add(unlockInfo);
+                    switch (compareType)
+                    {
+                        case Comparison.Distance:
+                            unlockInfo = firstUnlocks.FirstOrDefault(u => u.User == user);
+                            if (unlockInfo == null)
+                                continue;
+
+                            unlockInfo.SecondUnlock = unlock.GetField("DateAwarded").DateTimeValue.GetValueOrDefault();
+                            unlockInfo.Distance = unlockInfo.SecondUnlock - unlockInfo.FirstUnlock;
+                            unlocks.Add(unlockInfo);
+                            break;
+
+                        case Comparison.FirstWithoutSecond:
+                            firstUnlocks.RemoveAll(u => u.User == user);
+                            break;
+
+                        case Comparison.SecondWithoutFirst:
+                            unlockInfo = firstUnlocks.FirstOrDefault(u => u.User == user);
+                            if (unlockInfo != null)
+                                continue;
+
+                            unlockInfo = new UnlockInfo { User = user };
+                            unlockInfo.SecondUnlock = unlock.GetField("DateAwarded").DateTimeValue.GetValueOrDefault();
+                            unlockInfo.Distance = unlockInfo.SecondUnlock - unlockInfo.FirstUnlock;
+                            unlocks.Add(unlockInfo);
+                            break;
+                    }
                 }
-            }
+            } while (count == 500);
 
             Progress.Current++;
 
@@ -194,6 +282,21 @@ namespace RATools.ViewModels
                 MedianUnlockDistance = unlocks[unlocks.Count / 2].FormattedDistance;
             else
                 MedianUnlockDistance = "n/a";
+
+            switch (compareType)
+            {
+                case Comparison.Distance:
+                    SummaryText = String.Format("{0} users unlocked both achievements. Median unlock distance: {1}", UnlockCount, MedianUnlockDistance);
+                    break;
+
+                case Comparison.FirstWithoutSecond:
+                    SummaryText = String.Format("{0} users unlocked {1}, but not {2}", UnlockCount, FirstAchievementName, SecondAchievementName);
+                    break;
+
+                case Comparison.SecondWithoutFirst:
+                    SummaryText = String.Format("{0} users unlocked {1}, but not {2}", UnlockCount, SecondAchievementName, FirstAchievementName);
+                    break;
+            }
 
             Progress.Label = String.Empty;
         }
