60566bb8e5bb4f9396019a67620461dd369538dc
Jamiras <jamirasmt@gmail.com>
2025-07-07 08:36:09 -0600

    support subset id in local assets

---
+++ Source/Data/AchievementSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace RATools.Data
+{
+    [DebuggerDisplay("{Title} ({OwnerSetId})")]
+    public class AchievementSet : AssetBase
+    {
+        /// <summary>
+        /// Gets or sets the type classification of the achievement.
+        /// </summary>
+        public AchievementSetType Type { get; set; }
+    }
+}
