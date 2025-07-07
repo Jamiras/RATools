98fc747884e7cf6650b99890142ad10a55029b03
Jamiras <jamirasmt@gmail.com>
2025-07-07 07:18:41 -0600

    support achievementsets patch data

--- Source/Parser/PublishedAssets.cs
+++ Source/Parser/PublishedAssets.cs
@@ -39,6 +39,8 @@ namespace RATools.Parser
         private readonly IFileSystemService _fileSystemService;
         private readonly string _filename;
 
+        public int GameId { get; private set; }
+
         /// <summary>
         /// Gets the title of the associated game.
         /// </summary>
@@ -68,9 +70,10 @@ namespace RATools.Parser
 
         public RichPresence RichPresence { get; private set; }
 
+        private readonly DateTime _unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
         private void Read()
         {
-            var unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             _achievements.Clear();
             _leaderboards.Clear();
             RichPresence = null;
@@ -83,78 +86,128 @@ namespace RATools.Parser
                 var publishedData = new JsonObject(stream);
                 Title = publishedData.GetField("Title").StringValue;
 
-                var publishedAchievements = publishedData.GetField("Achievements");
+                var sets = publishedData.GetField("Sets");
+                if (sets.Type == JsonFieldType.ObjectArray)
+                {
+                    GameId = publishedData.GetField("GameId").IntegerValue.GetValueOrDefault();
+
+                    ReadSets(sets);
+                }
+                else
+                {
+                    GameId = publishedData.GetField("ID").IntegerValue.GetValueOrDefault();
+
+                    var publishedAchievements = publishedData.GetField("Achievements");
+                    if (publishedAchievements.Type == JsonFieldType.ObjectArray)
+                        ReadAchievements(publishedAchievements, GameId, 0);
+
+                    var publishedLeaderboards = publishedData.GetField("Leaderboards");
+                    if (publishedLeaderboards.Type == JsonFieldType.ObjectArray)
+                        ReadLeaderboards(publishedLeaderboards, GameId, 0);
+                }
+
+                var publishedRichPresence = publishedData.GetField("RichPresencePatch");
+                if (publishedRichPresence.Type == JsonFieldType.String)
+                { 
+                    RichPresence = new RichPresence
+                    {
+                        Script = publishedRichPresence.StringValue,
+                        OwnerGameId = GameId,
+                    };
+
+                    var richPresenceGameId = publishedData.GetField("RichPresenceGameId");
+                    if (richPresenceGameId.Type == JsonFieldType.Integer)
+                        RichPresence.OwnerGameId = richPresenceGameId.IntegerValue.GetValueOrDefault();
+                }
+            }
+        }
+
+        private void ReadSets(JsonField sets)
+        {
+            foreach (var set in sets.ObjectArrayValue)
+            {
+                var gameId = set.GetField("GameId").IntegerValue.GetValueOrDefault();
+                var setId = set.GetField("AchievementSetId").IntegerValue.GetValueOrDefault();
+
+                var publishedAchievements = set.GetField("Achievements");
                 if (publishedAchievements.Type == JsonFieldType.ObjectArray)
-                {
-                    foreach (var publishedAchievement in publishedAchievements.ObjectArrayValue)
-                    {
-                        var builder = new AchievementBuilder();
-                        builder.Id = publishedAchievement.GetField("ID").IntegerValue.GetValueOrDefault();
-                        builder.Title = publishedAchievement.GetField("Title").StringValue;
-                        builder.Description = publishedAchievement.GetField("Description").StringValue;
-                        builder.Points = publishedAchievement.GetField("Points").IntegerValue.GetValueOrDefault();
-                        builder.BadgeName = publishedAchievement.GetField("BadgeName").StringValue;
-                        builder.ParseRequirements(Tokenizer.CreateTokenizer(publishedAchievement.GetField("MemAddr").StringValue));
-                        builder.Category = publishedAchievement.GetField("Flags").IntegerValue.GetValueOrDefault();
+                    ReadAchievements(publishedAchievements, gameId, setId);
 
-                        var typeField = publishedAchievement.GetField("Type");
-                        if (!String.IsNullOrEmpty(typeField.StringValue))
-                            builder.Type = Achievement.ParseType(typeField.StringValue);
-
-                        var builtAchievement = builder.ToAchievement();
-                        builtAchievement.Published = unixEpoch.AddSeconds(publishedAchievement.GetField("Created").IntegerValue.GetValueOrDefault());
-                        builtAchievement.LastModified = unixEpoch.AddSeconds(publishedAchievement.GetField("Modified").IntegerValue.GetValueOrDefault());
-
-                        if (builtAchievement.Category == 5 || builtAchievement.Category == 3)
-                            _achievements.Add(builtAchievement);
-                    }
-                }
-
-                var publishedLeaderboards = publishedData.GetField("Leaderboards");
+                var publishedLeaderboards = set.GetField("Leaderboards");
                 if (publishedLeaderboards.Type == JsonFieldType.ObjectArray)
+                    ReadLeaderboards(publishedLeaderboards, gameId, setId);
+            }
+        }
+
+        private void ReadAchievements(JsonField publishedAchievements, int gameId, int setId)
+        {
+            foreach (var publishedAchievement in publishedAchievements.ObjectArrayValue)
+            {
+                var builder = new AchievementBuilder();
+                builder.Id = publishedAchievement.GetField("ID").IntegerValue.GetValueOrDefault();
+                builder.Title = publishedAchievement.GetField("Title").StringValue;
+                builder.Description = publishedAchievement.GetField("Description").StringValue;
+                builder.Points = publishedAchievement.GetField("Points").IntegerValue.GetValueOrDefault();
+                builder.BadgeName = publishedAchievement.GetField("BadgeName").StringValue;
+                builder.ParseRequirements(Tokenizer.CreateTokenizer(publishedAchievement.GetField("MemAddr").StringValue));
+                builder.Category = publishedAchievement.GetField("Flags").IntegerValue.GetValueOrDefault();
+
+                var typeField = publishedAchievement.GetField("Type");
+                if (!String.IsNullOrEmpty(typeField.StringValue))
+                    builder.Type = Achievement.ParseType(typeField.StringValue);
+
+                var builtAchievement = builder.ToAchievement();
+                builtAchievement.Published = _unixEpoch.AddSeconds(publishedAchievement.GetField("Created").IntegerValue.GetValueOrDefault());
+                builtAchievement.LastModified = _unixEpoch.AddSeconds(publishedAchievement.GetField("Modified").IntegerValue.GetValueOrDefault());
+                builtAchievement.OwnerGameId = gameId;
+                builtAchievement.OwnerSetId = setId;
+
+                if (builtAchievement.Category == 5 || builtAchievement.Category == 3)
+                    _achievements.Add(builtAchievement);
+            }
+        }
+
+        private void ReadLeaderboards(JsonField publishedLeaderboards, int gameId, int setId)
+        {
+            foreach (var publishedLeaderboard in publishedLeaderboards.ObjectArrayValue)
+            {
+                var leaderboard = new Leaderboard();
+                leaderboard.Id = publishedLeaderboard.GetField("ID").IntegerValue.GetValueOrDefault();
+                leaderboard.Title = publishedLeaderboard.GetField("Title").StringValue;
+                leaderboard.Description = publishedLeaderboard.GetField("Description").StringValue;
+                leaderboard.Format = Leaderboard.ParseFormat(publishedLeaderboard.GetField("Format").StringValue);
+                leaderboard.LowerIsBetter = publishedLeaderboard.GetField("LowerIsBetter").BooleanValue;
+
+                var mem = publishedLeaderboard.GetField("Mem").StringValue;
+                var tokenizer = Tokenizer.CreateTokenizer(mem);
+                while (tokenizer.NextChar != '\0')
                 {
-                    foreach (var publishedLeaderboard in publishedLeaderboards.ObjectArrayValue)
-                    {
-                        var leaderboard = new Leaderboard();
-                        leaderboard.Id = publishedLeaderboard.GetField("ID").IntegerValue.GetValueOrDefault();
-                        leaderboard.Title = publishedLeaderboard.GetField("Title").StringValue;
-                        leaderboard.Description = publishedLeaderboard.GetField("Description").StringValue;
-                        leaderboard.Format = Leaderboard.ParseFormat(publishedLeaderboard.GetField("Format").StringValue);
-                        leaderboard.LowerIsBetter = publishedLeaderboard.GetField("LowerIsBetter").BooleanValue;
+                    var part = tokenizer.ReadTo("::");
+                    if (part.StartsWith("STA:"))
+                        leaderboard.Start = Trigger.Deserialize(part.Substring(4));
+                    else if (part.StartsWith("CAN:"))
+                        leaderboard.Cancel = Trigger.Deserialize(part.Substring(4));
+                    else if (part.StartsWith("SUB:"))
+                        leaderboard.Submit = Trigger.Deserialize(part.Substring(4));
+                    else if (part.StartsWith("VAL:"))
+                        leaderboard.Value = Value.Deserialize(part.Substring(4));
 
-                        var mem = publishedLeaderboard.GetField("Mem").StringValue;
-                        var tokenizer = Tokenizer.CreateTokenizer(mem);
-                        while (tokenizer.NextChar != '\0')
-                        {
-                            var part = tokenizer.ReadTo("::");
-                            if (part.StartsWith("STA:"))
-                                leaderboard.Start = Trigger.Deserialize(part.Substring(4));
-                            else if (part.StartsWith("CAN:"))
-                                leaderboard.Cancel = Trigger.Deserialize(part.Substring(4));
-                            else if (part.StartsWith("SUB:"))
-                                leaderboard.Submit = Trigger.Deserialize(part.Substring(4));
-                            else if (part.StartsWith("VAL:"))
-                                leaderboard.Value = Value.Deserialize(part.Substring(4));
-
-                            tokenizer.Advance(2);
-                        }
-
-                        if (leaderboard.Start == null)
-                            leaderboard.Start = new Trigger();
-                        if (leaderboard.Cancel == null)
-                            leaderboard.Cancel = new Trigger();
-                        if (leaderboard.Submit == null)
-                            leaderboard.Submit = new Trigger();
-                        if (leaderboard.Value == null)
-                            leaderboard.Value = new Value();
-
-                        _leaderboards.Add(leaderboard);
-                    }
-
-                    var publishedRichPresence = publishedData.GetField("RichPresencePatch");
-                    if (publishedRichPresence.Type == JsonFieldType.String)
-                        RichPresence = new RichPresence { Script = publishedRichPresence.StringValue };
+                    tokenizer.Advance(2);
                 }
+
+                if (leaderboard.Start == null)
+                    leaderboard.Start = new Trigger();
+                if (leaderboard.Cancel == null)
+                    leaderboard.Cancel = new Trigger();
+                if (leaderboard.Submit == null)
+                    leaderboard.Submit = new Trigger();
+                if (leaderboard.Value == null)
+                    leaderboard.Value = new Value();
+
+                leaderboard.OwnerGameId = gameId;
+                leaderboard.OwnerSetId = setId;
+
+                _leaderboards.Add(leaderboard);
             }
         }
     }
